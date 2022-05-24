558dd4babf6f0c38cacdef0c65870a64a1db632a
doverchivost <doverchivostus@gmail.com>
2022-05-24 16:26:20 +0500

    Connection with timeouts (doesn't help tho)

--- src/Singletons/Telegram.java
+++ src/Singletons/Telegram.java
@@ -2,6 +2,9 @@ package Singletons;
 
 import Constants.Constants;
 import com.pengrad.telegrambot.TelegramBot;
+import okhttp3.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
 
 public class Telegram {
     private static volatile TelegramBot telegramBot;
@@ -12,7 +15,13 @@ public class Telegram {
             synchronized (Telegram.class) {
                 localBot = telegramBot;
                 if (localBot == null) {
-                    telegramBot = localBot = new TelegramBot(Constants.telegram_bot_token);
+                    //telegramBot = localBot = new TelegramBot(Constants.telegram_bot_token);
+                    telegramBot = localBot = new TelegramBot.Builder(Constants.telegram_bot_token)
+                            .okHttpClient(new OkHttpClient.Builder()
+                                    .readTimeout(60, TimeUnit.SECONDS)
+                                    .connectTimeout(60, TimeUnit.SECONDS)
+                                    .build())
+                            .build();
                 }
             }
         }
